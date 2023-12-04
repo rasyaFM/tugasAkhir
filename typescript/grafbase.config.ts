4760339335fef1a5b9bf45f7ea57603b2c03ad18
fadhli <jackfadhli32@gmail.com>
2023-12-04 23:45:44 +0700

    first commit

---
+++ typescript/grafbase.config.ts
@@ -0,0 +1,25 @@
+import { g, auth ,config } from '@grafbase/sdk'
+
+const User = g.model('User', {
+  id: g.id()
+  name: g.string().length({min:10, max:25}),
+  email: g.string().unique(['id']),
+  avatarUrl: g.url(),
+  description: g.string().optional(),
+  projects: g.relation(() => Project).list().optional(),
+})
+
+const Project = g.model('Project', {
+  title: g.string().length({min:5}),
+  description: g.string(),
+  image: g.url(),
+  category: g.string().search(),
+  ownBy: g.relation(() => User),
+})
+
+
+
+export default config({
+  schema: g
+  
+})
